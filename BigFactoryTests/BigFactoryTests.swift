d0c45250e32e65bce4192792177b2759cb7d72ca
Huang <359503893@qq.com>
2015-12-07 14:17:11 +0800

    Initial Commit

---
+++ BigFactoryTests/BigFactoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  BigFactoryTests.swift
+//  BigFactoryTests
+//
+//  Created by huangqiang on 12/7/15.
+//  Copyright © 2015 BigLaugh. All rights reserved.
+//
+
+import XCTest
+@testable import BigFactory
+
+class BigFactoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
